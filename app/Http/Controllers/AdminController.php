a31f4e155c3a36537696d16f82bfa388a2059ffe
Sergimr27 <sergimarinribes@gmail.com>
2024-04-26 09:41:31 +0200

    cambiar estado usuario

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use App\Models\Rol;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
@@ -36,5 +37,27 @@ class AdminController extends Controller
     {
         $roles = Rol::all();
         return response()->json($roles);
+    }
+    public function cambiarEstado($id)
+    {
+        try {
+        
+            DB::beginTransaction();
+ 
+            $user = User::lockForUpdate()->findOrFail($id);
+
+            $user->habilitado = $user->habilitado == 1 ? 0 : 1;
+            $user->save();
+
+            DB::commit();
+
+          
+            return response()->json(['success' => true]);
+        } catch (\Exception $e) {
+           
+            DB::rollBack();
+
+            return response()->json(['success' => false, 'message' => $e->getMessage()], 500);
+        }
     }
 }
--- public/js/adminUsers.js
+++ public/js/adminUsers.js
@@ -34,7 +34,7 @@ function ListarUsuarios(valor, rol) {
                 str += "<td>" + item.name + "</td>";
                 str += "<td>" + item.email + "</td>";
                 str += "<td>" + item.rol.name + "</td>";
-                str += "<td><button onclick='CambiarEstado(" + item.id + ", " + item.habilitado + ")'>" + (item.habilitado == 1 ? "Habilitado" : "Deshabilitado") + "</button></td>";
+                str += "<td><button onclick='CambiarEstado(" + item.id + ")'>" + (item.habilitado == 1 ? "Habilitado" : "Deshabilitado") + "</button></td>";
                 str += "<td><button onclick='Editar(" + item.id + ")'>Editar</button></td>";
                 str += "<td><button onclick='Eliminar(" + item.id + ")'>Eliminar</button></td>";
                 str += "</tr>";
@@ -70,4 +70,117 @@ function obtenerRoles() {
             });
         })
         .catch(error => console.error('Error al obtener los marcadores:', error));
+}
+
+/* function Eliminar(id) {
+    var rol = document.getElementById("rol").value;
+    var csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+    Swal.fire({
+        title: "Eliminar usuario",
+        text: `¿Seguro que desea eliminar el usuario?`,
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#d33",
+        cancelButtonColor: "#0052CC",
+        confirmButtonText: "Eliminar",
+        cancelButtonText: "Cancelar"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            fetch(`admin/crudusuarios/${id}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'X-CSRF-TOKEN': csrfToken
+                    }
+                })
+                .then(response => {
+                    if (!response.ok) {
+                        EndLoading();
+                        Swal.fire({
+                            title: "An error has occurred.",
+                            text: "Could not delete.",
+                            icon: "error"
+                        });
+                        return;
+                    }
+                    return response.json();
+                })
+                .then(data => {
+
+                    if (data.success == true) {
+                        Swal.fire({
+                            title: "Borrado",
+                            text: "Usuario eliminado correctamente",
+                            icon: "success"
+                        }).then(() => {
+                            listarUsers('', rol);
+
+                        });
+                    } else {
+                        Swal.fire({
+                            title: "Error al eliminar el usuario",
+                            text: data.message,
+                            icon: "error"
+                        });
+                    }
+                })
+                .catch(error => console.error('Error al eliminar el usuario:', error));
+        }
+    });
+} */
+
+
+function CambiarEstado(id) {
+    var rol = document.getElementById("rol").value;
+    var csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+    Swal.fire({
+        title: "Cambiar estado usuario",
+        text: `¿Seguro que desea cambiar el estado?`,
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#d33",
+        cancelButtonColor: "#0052CC",
+        confirmButtonText: "Cambiar",
+        cancelButtonText: "Cancelar"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            fetch(`admin/crudusuarios/cambiarestado/${id}`, {
+                    method: 'POST',
+                    headers: {
+                        'X-CSRF-TOKEN': csrfToken
+                    }
+                })
+                .then(response => {
+                    if (!response.ok) {
+                        EndLoading();
+                        Swal.fire({
+                            title: "Ha ocurrido un error al cambiar el estado.",
+                            text: "No se ha podido cambiar",
+                            icon: "error"
+                        });
+                        return;
+                    }
+                    return response.json();
+                })
+                .then(data => {
+
+                    if (data.success == true) {
+                        Swal.fire({
+                            title: "Cambiar Estado",
+                            text: "Estado del usuario actualizado correctamente",
+                            icon: "success"
+                        }).then(() => {
+                            ListarUsuarios('', rol);
+
+                        });
+                    } else {
+                        Swal.fire({
+                            title: "Error al cambiar el estado del usuario",
+                            text: data.message,
+                            icon: "error"
+                        });
+                    }
+                })
+                .catch(error => console.error('Error al cambiar el estado del usuario:', error));
+        }
+    });
 }
